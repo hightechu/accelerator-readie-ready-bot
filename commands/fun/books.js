3d6c4f88e1174303370fad728f21b327d570d5a1
Bridget Weston <41244374+brweston@users.noreply.github.com>
2021-08-26 15:33:00 -0700

    Update books.js

--- commands/fun/books.js
+++ commands/fun/books.js
@@ -1,5 +1,4 @@
-const axios = require('axios');
-// Ping Module
+const fetch = require('node-fetch');
 module.exports = {
     // Name of Command
     name: 'books',
@@ -16,18 +15,30 @@ module.exports = {
         var baseUrl = "https://www.googleapis.com/books/v1/volumes?q=flowers"
         //var query = args.toString()
         //var queryUrl = baseUrl + query
-        axios.get(baseUrl)
-  .then(function (response) {
-    // handle success
-   message.channel.send(response.data);
-  })
-  .catch(function (error) {
-    // handle error
-    message.channel.send(error)
-  })
-  .then(function () {
-    // always executed
-    message.channel.send("There was an error! Oh no!")
-  });
+        fetch('https://covidapi.info/api/v1/global/2021-08-01')
+        .then(response => response.json())
+        .then((data) => {
+            const confirmedCases = data.result.confirmed;
+            const deathCases = data.result.deaths;
+            const recoveredCases = data.result.recovered;
+
+            const embed = new Discord.MessageEmbed()
+                .setColor('#20C19E')
+                .setTitle('COVID 19 - Global Recovery Chart')
+                .setDescription('Viewing Data for: ' + date)
+                .addFields(
+                    { name: 'Confirmed', value: confirmedCases, inline: true },
+                    { name: 'Deaths', value: deathCases, inline: true },
+                    { name: 'Recovered', value: recoveredCases, inline: true },
+                )
+                .setTimestamp()
+                .setFooter('Powered by COVID API - https://covidapi.info/');
+
+            // Send Message
+            message.channel.send(embed);
+        }).catch((error) => {
+            console.log(error);
+            message.channel.send('An Error Occured. Please verify that you inputed a correct date.');
+        });
     },
 };
