2ccd4521dc310129a7f88fa44873cdf10b406172
Venkatesh A <venkatesha@Roshinis-MacBook-Air.local>
2021-08-27 14:30:39 -0700

    created morebyauthor file

---
+++ commands/fun/books.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+// Ping Module
+module.exports = {
+    // Name of Command
+    name: 'books',
+    // Description of Command
+    description: 'Ping!',
+    // Guild - TRUE
+    guildOnly: true,
+    args: true,
+    // Cooldown
+    cooldown: 50,
+    // Execute Command - Parameters: message
+    execute(message, args) {
+        // Send Message
+        var baseUrl = "https://www.googleapis.com/books/v1/volumes?q=flowers"
+        //var query = args.toString()
+        //var queryUrl = baseUrl + query
+        axios.get(baseUrl)
+  .then(function (response) {
+    // handle success
+   message.channel.send(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+    },
+};
